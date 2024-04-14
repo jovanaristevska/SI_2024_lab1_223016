19644ce235639fbcafe32db38e3e3e01017afcb4
jovanaristevska <ristevskaj@hotmail.com>
2024-04-14 14:43:07 +0200

    Call method filterOddLengthStrings in main class

--- SILab1Main.java
+++ SILab1Main.java
@@ -2,8 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class SILab1Main {
-    public static void main(String[] args) {
+public class SILab1Main {    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
@@ -13,6 +12,8 @@ public class SILab1Main {
         for (int i = 0; i < n; i++) {
             list.add(sc.next());
         }
+
         System.out.println(SILab1_1.filterOddLengthStrings(list));
+
     }
 }
